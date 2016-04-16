b67aca194c752f2c70a575c2f157b1b1d41bb9b3
VJan-fin <vik>
2016-04-16 17:56:21 +0200

    Project created

---
+++ GenskaRegulacijaAVPR1a/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GenskaRegulacijaAVPR1a
+{
+    partial class NucleusForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NucleusForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 612);
+            this.Name = "NucleusForm";
+            this.Text = "Генска регулација на AVPR1a";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
