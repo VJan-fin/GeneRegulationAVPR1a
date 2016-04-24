797ba9e4dacaac5d133f51ee9e55b31dd2222f9e
laverte <elena.dimitriova@gmail.com>
2016-04-24 18:34:31 +0200

    Added files via upload

---
+++ GenskaRegulacijaAVPR1a/Form2.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace GenskaRegulacijaAVPR1a
+{
+    partial class mRNAForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mRNAForm));
+            this.pbRibosome = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBarDex = new System.Windows.Forms.TrackBar();
+            this.trackBarCort = new System.Windows.Forms.TrackBar();
+            this.trackBarAldo = new System.Windows.Forms.TrackBar();
+            this.lblAldosterone = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            this.lblCorticosterone = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            this.lblDexamethasone = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            this.lblGlucocorticoids = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            this.lblRibosome = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            this.lblGeneNamemRNA = new GenskaRegulacijaAVPR1a.ClickableLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRibosome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarAldo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbRibosome
+            // 
+            this.pbRibosome.Image = ((System.Drawing.Image)(resources.GetObject("pbRibosome.Image")));
+            this.pbRibosome.Location = new System.Drawing.Point(593, 67);
+            this.pbRibosome.Name = "pbRibosome";
+            this.pbRibosome.Size = new System.Drawing.Size(70, 70);
+            this.pbRibosome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRibosome.TabIndex = 16;
+            this.pbRibosome.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(27, 429);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(691, 105);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBarDex
+            // 
+            this.trackBarDex.Location = new System.Drawing.Point(264, 141);
+            this.trackBarDex.Name = "trackBarDex";
+            this.trackBarDex.Size = new System.Drawing.Size(100, 42);
+            this.trackBarDex.TabIndex = 44;
+            this.trackBarDex.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // trackBarCort
+            // 
+            this.trackBarCort.Location = new System.Drawing.Point(264, 189);
+            this.trackBarCort.Name = "trackBarCort";
+            this.trackBarCort.Size = new System.Drawing.Size(100, 42);
+            this.trackBarCort.TabIndex = 45;
+            this.trackBarCort.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // trackBarAldo
+            // 
+            this.trackBarAldo.Location = new System.Drawing.Point(264, 237);
+            this.trackBarAldo.Name = "trackBarAldo";
+            this.trackBarAldo.Size = new System.Drawing.Size(100, 42);
+            this.trackBarAldo.TabIndex = 46;
+            this.trackBarAldo.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            // 
+            // lblAldosterone
+            // 
+            this.lblAldosterone.AutoSize = true;
+            this.lblAldosterone.BackColor = System.Drawing.Color.Transparent;
+            this.lblAldosterone.ExplanationText = null;
+            this.lblAldosterone.Font = new System.Drawing.Font("Calibri", 11F);
+            this.lblAldosterone.FontSize = 0F;
+            this.lblAldosterone.ForeColor = System.Drawing.Color.White;
+            this.lblAldosterone.Location = new System.Drawing.Point(149, 248);
+            this.lblAldosterone.Name = "lblAldosterone";
+            this.lblAldosterone.Size = new System.Drawing.Size(85, 18);
+            this.lblAldosterone.TabIndex = 43;
+            this.lblAldosterone.Text = "Aldosterone";
+            this.lblAldosterone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCorticosterone
+            // 
+            this.lblCorticosterone.AutoSize = true;
+            this.lblCorticosterone.BackColor = System.Drawing.Color.Transparent;
+            this.lblCorticosterone.ExplanationText = null;
+            this.lblCorticosterone.Font = new System.Drawing.Font("Calibri", 11F);
+            this.lblCorticosterone.FontSize = 0F;
+            this.lblCorticosterone.ForeColor = System.Drawing.Color.White;
+            this.lblCorticosterone.Location = new System.Drawing.Point(149, 202);
+            this.lblCorticosterone.Name = "lblCorticosterone";
+            this.lblCorticosterone.Size = new System.Drawing.Size(99, 18);
+            this.lblCorticosterone.TabIndex = 41;
+            this.lblCorticosterone.Text = "Corticosterone";
+            this.lblCorticosterone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDexamethasone
+            // 
+            this.lblDexamethasone.AutoSize = true;
+            this.lblDexamethasone.BackColor = System.Drawing.Color.Transparent;
+            this.lblDexamethasone.ExplanationText = null;
+            this.lblDexamethasone.Font = new System.Drawing.Font("Calibri", 11F);
+            this.lblDexamethasone.FontSize = 0F;
+            this.lblDexamethasone.ForeColor = System.Drawing.Color.White;
+            this.lblDexamethasone.Location = new System.Drawing.Point(149, 154);
+            this.lblDexamethasone.Name = "lblDexamethasone";
+            this.lblDexamethasone.Size = new System.Drawing.Size(109, 18);
+            this.lblDexamethasone.TabIndex = 39;
+            this.lblDexamethasone.Text = "Dexamethasone";
+            this.lblDexamethasone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGlucocorticoids
+            // 
+            this.lblGlucocorticoids.AutoSize = true;
+            this.lblGlucocorticoids.BackColor = System.Drawing.Color.Transparent;
+            this.lblGlucocorticoids.ExplanationText = null;
+            this.lblGlucocorticoids.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGlucocorticoids.FontSize = 0F;
+            this.lblGlucocorticoids.ForeColor = System.Drawing.Color.White;
+            this.lblGlucocorticoids.Location = new System.Drawing.Point(148, 96);
+            this.lblGlucocorticoids.Name = "lblGlucocorticoids";
+            this.lblGlucocorticoids.Size = new System.Drawing.Size(150, 23);
+            this.lblGlucocorticoids.TabIndex = 21;
+            this.lblGlucocorticoids.Text = "Глукокортикоиди";
+            this.lblGlucocorticoids.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRibosome
+            // 
+            this.lblRibosome.BackColor = System.Drawing.Color.Transparent;
+            this.lblRibosome.ExplanationText = null;
+            this.lblRibosome.Font = new System.Drawing.Font("Calibri", 14F);
+            this.lblRibosome.FontSize = 0F;
+            this.lblRibosome.ForeColor = System.Drawing.Color.White;
+            this.lblRibosome.Location = new System.Drawing.Point(553, 28);
+            this.lblRibosome.Name = "lblRibosome";
+            this.lblRibosome.Size = new System.Drawing.Size(153, 37);
+            this.lblRibosome.TabIndex = 17;
+            this.lblRibosome.Text = "Рибозом";
+            this.lblRibosome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGeneNamemRNA
+            // 
+            this.lblGeneNamemRNA.BackColor = System.Drawing.Color.Transparent;
+            this.lblGeneNamemRNA.ExplanationText = null;
+            this.lblGeneNamemRNA.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGeneNamemRNA.FontSize = 0F;
+            this.lblGeneNamemRNA.ForeColor = System.Drawing.Color.White;
+            this.lblGeneNamemRNA.Location = new System.Drawing.Point(32, 26);
+            this.lblGeneNamemRNA.Name = "lblGeneNamemRNA";
+            this.lblGeneNamemRNA.Size = new System.Drawing.Size(297, 29);
+            this.lblGeneNamemRNA.TabIndex = 15;
+            this.lblGeneNamemRNA.Text = "AVPR1a mRNA молекула";
+            this.lblGeneNamemRNA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mRNAForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(984, 612);
+            this.Controls.Add(this.trackBarAldo);
+            this.Controls.Add(this.trackBarCort);
+            this.Controls.Add(this.trackBarDex);
+            this.Controls.Add(this.lblAldosterone);
+            this.Controls.Add(this.lblCorticosterone);
+            this.Controls.Add(this.lblDexamethasone);
+            this.Controls.Add(this.lblGlucocorticoids);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblRibosome);
+            this.Controls.Add(this.pbRibosome);
+            this.Controls.Add(this.lblGeneNamemRNA);
+            this.Name = "mRNAForm";
+            this.Text = "Генска регулација на AVPR1a";
+            ((System.ComponentModel.ISupportInitialize)(this.pbRibosome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarDex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarAldo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ClickableLabel lblGeneNamemRNA;
+        private System.Windows.Forms.PictureBox pbRibosome;
+        private ClickableLabel lblRibosome;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private ClickableLabel lblGlucocorticoids;
+        private ClickableLabel lblAldosterone;
+        private ClickableLabel lblCorticosterone;
+        private ClickableLabel lblDexamethasone;
+        private System.Windows.Forms.TrackBar trackBarDex;
+        private System.Windows.Forms.TrackBar trackBarCort;
+        private System.Windows.Forms.TrackBar trackBarAldo;
+    }
+}
